17cb000c7d1749ea299afb5967d1a876a2a65f8d
Pieter K de Villiers <pieter@hey.com>
2023-09-10 16:19:25 +0100

    Intial Commit

--- App.js
+++ App.js
@@ -4,12 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open  App.js!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
